842be2f506f61c38cbebae30df3ba007b2dc45df
AinaMontalban <aina.montalban@alum.esci.upf.edu>
2022-02-14 10:57:33 +0100

    uploaded data & results

---
+++ README.md
@@ -0,0 +1,22 @@
+# Exercise Sjogren Syndrom (SS)
+
+## Author
+
+* Aina Montalban 
+
+## Description
+
+Analysis of gene expression profiles of samples of minor salivary glands of patients with _Sjogren's syndrome_. 
+
+## Repository content
+
+* The folder [results]() contains:
+    + The [html file]() with the code and the results of the exercise
+    + The [images]() folder with the dendogram and the PCA plot 
+* The folder [scripts]() contains:
+	+ The Rmarkdown that builds the [html file]()
+	+ The R script from the Rmarkdown
+* The folder [data]() contains:
+	+ CEL files of the 15 samples
+	+ targets.csv: created csv file from the phenotype data 
+
---
+++ scripts/exercise_sjogren_syndrome.R
@@ -0,0 +1,161 @@
+## ----setup, include=FALSE-----------------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+#install.packages("prettydoc")
+
+
+## ----message=FALSE, warning=FALSE---------------------------------------------------------------
+workingDir <- getwd()
+data_dir <- file.path(workingDir, "data")
+results_dir <- file.path(workingDir, "results")
+
+
+## ----message=FALSE, warning=FALSE---------------------------------------------------------------
+# load packages
+library(oligo)
+library(Biobase)
+library(gplots)
+library(ggplot2)
+library(ggrepel)
+library(affy)
+library(GEOquery)
+library(hgu133plus2cdf)
+library(ggdendro)
+
+
+## ----message=FALSE, warning=FALSE---------------------------------------------------------------
+CELfiles_fullName <- list.celfiles(file.path(data_dir), full.names=TRUE) # store the full path
+
+
+## ----message=FALSE, warning=FALSE, results='hide'-----------------------------------------------
+# store a variable name raw data as the data is not normalized
+raw_data <- read.celfiles(CELfiles_fullName)
+
+
+## -----------------------------------------------------------------------------------------------
+name_sample <- sampleNames(raw_data)
+pData(raw_data)$name_sample <- name_sample
+sampleNames <- sub(".*_", "", name_sample)
+sampleNames <- sub(".CEL.gz$", "", name_sample)
+sampleNames(raw_data) <- sampleNames
+
+
+## ----eval=FALSE---------------------------------------------------------------------------------
+## gse <- getGEO(filename = file.path(data_dir, "GSE23117_series_matrix.txt.gz"),
+##               GSEMatrix = FALSE)
+
+
+## ----include=FALSE------------------------------------------------------------------------------
+gse <- readRDS(file.path(data_dir, "my_gse.rds")) # used bc of a problem in getGEO function
+
+
+## -----------------------------------------------------------------------------------------------
+pheno_data <- pData(phenoData(gse))
+
+
+## -----------------------------------------------------------------------------------------------
+targets <- pheno_data[, c("geo_accession", "disease status:ch1")]
+targets$group <- c(rep(1, 2), rep(0,4), rep(1,9))
+targets$shortName <- c(rep("advanced_SS", 2), rep("control", 4), 
+                       rep("control_SS", 1), rep("early_SS", 5), 
+                       rep("moderate_SS", 3))
+
+
+## -----------------------------------------------------------------------------------------------
+# store a CSV file
+write.csv(targets, file.path(data_dir, "targets.csv"), row.names = FALSE)
+
+
+## -----------------------------------------------------------------------------------------------
+columnDesc <-  data.frame(labelDescription= c("geo_accession",
+                                              "disease status:ch1", "group", 
+                                              "shortName"))
+myAnnotDF <- new("AnnotatedDataFrame", data=targets, varMetadata= columnDesc)
+
+
+## -----------------------------------------------------------------------------------------------
+affy_object <- read.affybatch(filenames = CELfiles_fullName, 
+                              phenoData = myAnnotDF)
+show(affy_object)
+
+
+## ----message=FALSE, warning=FALSE---------------------------------------------------------------
+exp_norm <- affy::rma(affy_object)
+
+
+## -----------------------------------------------------------------------------------------------
+head(exprs(exp_norm))[,1:5]
+
+
+## -----------------------------------------------------------------------------------------------
+dist_eucl_clust <- hclust(dist(t(exprs(exp_norm))),
+                               method="average")
+dend <- as.dendrogram(dist_eucl_clust)
+
+
+## -----------------------------------------------------------------------------------------------
+dend_data <- dendro_data(dend, type = "rectangle")
+lbs <- label(dend_data)$label
+lst <- c()
+for (ele in lbs){
+    gName <- targets[targets$geo_accession==ele,"shortName"]
+    lst <- c(lst, gName)
+}
+dend_data$labels[, "label"] <- lst
+
+
+## -----------------------------------------------------------------------------------------------
+p1 <- ggplot(segment(dend_data)) + 
+      geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) +
+      coord_flip() +  scale_y_reverse(expand = c(0.2, 0.2))
+
+p1 <- p1 + theme(axis.line.y=element_blank(), axis.ticks.y=element_blank(),
+     axis.text.y=element_blank(), axis.title.y=element_blank(),
+     panel.background=element_rect(fill="white")) + labs(y="Height")
+
+p1 <- p1 + geom_text(data = label(dend_data), 
+              aes(x = x, y = y, label = label, color=label), 
+              size = 3, vjust=0.5, hjust=0, show.legend = FALSE) 
+p1
+
+## ----include=FALSE------------------------------------------------------------------------------
+png(file.path(results_dir, "images/dendrogram.png"))
+print(p1)
+dev.off()
+
+
+## ----include=FALSE------------------------------------------------------------------------------
+#showtext::showtext_auto()
+
+
+## -----------------------------------------------------------------------------------------------
+X <- t(exprs(exp_norm))
+head(X)[1:4, 1:5]
+
+
+## -----------------------------------------------------------------------------------------------
+pca <- prcomp(X, scale. = FALSE)
+Groups <- as.factor(targets$group)
+shortName <- targets$shortName
+
+
+## -----------------------------------------------------------------------------------------------
+loads <- round(pca$sdev^2/sum(pca$sdev^2)*100,1)
+pca_df <- data.frame(pca$x)
+
+
+## -----------------------------------------------------------------------------------------------
+p2 <- ggplot(pca_df, aes(x=PC1, y=PC2, color=Groups, label=shortName)) + 
+       geom_point(aes(color=Groups), size=3) +
+        scale_colour_discrete(name="Group", labels=c("Control", "SS"))
+
+p2 <- p2 + theme_classic() + labs(x=c(paste("PC1 (",loads[1],"%)")),
+                          y=c(paste("PC2 (",loads[2],"%)"))) 
+
+p2 <- p2 + geom_text_repel(show.legend = FALSE)
+p2
+
+## ----include=FALSE------------------------------------------------------------------------------
+png(file.path(results_dir, "images/pca.png"))
+print(p2)
+dev.off()
+
